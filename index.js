e3921d9f9ffa8067ddf0e24bc06e9aaa3803b63c
Markusha <legoanimation2022@gmail.com>
2025-04-29 22:27:11 +0300

    2

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('In').addEventListener('click', function() {
+    location.href = 'All/Log/Signin';
+});
+
+document.getElementById('Reg').addEventListener('click', function() {
+    location.href = 'All/Log/Signup';
+});
